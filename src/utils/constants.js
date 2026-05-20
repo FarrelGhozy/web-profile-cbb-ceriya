778757e1c5f6739241e352892dc64e050ed8b9ef
FarrelGhozy <farrelafif05@gmail.com>
2026-05-20 08:20:42 +0700

    fix: set Vite base to relative paths for GitHub Pages deployment
    - Add base: './' in vite.config.js so all asset paths are relative
    - Change all image src paths from absolute (/images/) to relative (images/)
    - Fix avatar paths in constants.js
    - Previously CSS/JS/images 404'd on GitHub Pages sub-path URLs
    
    Closes GitHub Pages deployment issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,20 +4,20 @@ export const testimonials = [
     quote: 'CBB CERIYA luar biasa! Anak saya jadi lebih percaya diri dan senang belajar setiap hari. Metode pembelajarannya sangat interaktif.',
     name: 'Ibu Siti',
     role: 'Orang Tua Ananda (TK A)',
-    avatar: '/images/avatar.svg',
+    avatar: 'images/avatar.svg',
   },
   {
     id: 2,
     quote: 'Guru-gurunya sangat sabar dan profesional. Saya melihat perkembangan signifikan pada kemampuan sosial anak saya.',
     name: 'Bapak Ahmad',
     role: 'Orang Tua Ananda (TK B)',
-    avatar: '/images/avatar.svg',
+    avatar: 'images/avatar.svg',
   },
   {
     id: 3,
     quote: 'Lingkungan sekolah yang aman dan nyaman membuat saya tenang meninggalkan anak. Fasilitasnya lengkap dan terawat.',
     name: 'Ibu Dewi',
     role: 'Orang Tua Ananda (Playgroup)',
-    avatar: '/images/avatar.svg',
+    avatar: 'images/avatar.svg',
   },
 ]
